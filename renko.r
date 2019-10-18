2e8058a42b96e130fb5761b527314c0ad91193a8
RenkoT97 <tjasa.renko@gmail.com>
2019-10-18 18:47:36 +0200

    Add files via upload

--- renko.r
+++ renko.r
@@ -1,21 +1,25 @@
 library(readr)
 library(dplyr)
 
-tabela1 <- read.csv("hist_EURIBOR_2010.csv")
+tabela1 <- read.csv("https://raw.githubusercontent.com/RenkoT97/FinancniPraktikum/master/hist_EURIBOR_2010.csv", stringsAsFactors=FALSE)
+tabela2 <- read.csv("https://raw.githubusercontent.com/RenkoT97/FinancniPraktikum/master/hist_EURIBOR_2011.csv", stringsAsFactors=FALSE)
+tabela3 <- read.csv("https://raw.githubusercontent.com/RenkoT97/FinancniPraktikum/master/hist_EURIBOR_2012.csv", stringsAsFactors=FALSE)
+
+#tabela1 <- read.csv("hist_EURIBOR_2010.csv")
 tabela1 <- t(tabela1)
 tabela1 <- tabela1[,c(4:15)]
 colnames(tabela1) <- tabela1[1,]
 tabela1 <- tabela1[-1, ]
 tabela1 <- tabela1[c(1,21,41,64,86,107,129,151,173,195,216,238),]
 
-tabela2 <- read.csv("hist_EURIBOR_2011.csv")
+#tabela2 <- read.csv("hist_EURIBOR_2011.csv")
 tabela2 <- t(tabela2)
 tabela2 <- tabela2[,c(4:15)]
 colnames(tabela2) <- tabela2[1,]
 tabela2 <- tabela2[-1, ]
 tabela2 <- tabela2[c(1,22,42,65,84,106,128,149,172,194,215,237),]
 
-tabela3 <- read.csv("hist_EURIBOR_2012.csv")
+#tabela3 <- read.csv("hist_EURIBOR_2012.csv")
 tabela3 <- t(tabela3)
 tabela3 <- tabela3[,c(4:15)]
 colnames(tabela3) <- tabela3[1,]
@@ -29,12 +33,14 @@ colnames(tabelaLTU) <- c("polletna", "enoletna")
 U<-12
 T<-6
 n<-36
-a <- 1/(U-T)
-k <- rep(1/(U-T),n)
-m <- rep((1+U)/(1+T),n)
-tabelaLTU <- cbind(tabelaLTU,k,m)
-#tabelaLTU$"terminskaobrestnamera" <- 1/(U-T)*((1+U)/(1+T)*(tabelaLTU[,c(2)])/tabelaLTU[,c(1)]-rep(1,n))
-tabelaLTU$"terminskaobrestnamera" <- tabelaLTU$"k"*(tabelaLTU$"m"*tabelaLTU$"polletna"/tabelaLTU$"enoletna")-1
+tabelaLTU[,1] <- as.numeric(tabelaLTU[,1])
+tabelaLTU[,2] <- as.numeric(tabelaLTU[,2])
+
+tabelaLTU <- as.numeric(tabelaLTU)
+sapply(tabelaLTU, class)
+
+terminskeobrestnemere <- rep(1,n)/(U-T)*((rep(1,n)+U*(tabelaLTU[,2]))/(rep(1,n)+T*(tabelaLTU[,1]))-rep(1,n))
+
 
 
 #ts.plot(tabelaLTU, xlab="meseci", ylab="obrestne mere")
