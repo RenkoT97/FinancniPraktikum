0538189dd8c8a5aba52ca788ecd6b8656b905a54
RenkoT97 <tjasa.renko@gmail.com>
2019-11-22 10:00:25 +0100

    Add files via upload

--- druga.r
+++ druga.r
@@ -7,27 +7,32 @@ podatki <- read.delim("https://raw.githubusercontent.com/RenkoT97/FinancniPrakti
 #pogostost <- count(podatki, vars = unikatne_vrednosti)
 
 visinaodskodnine <- as.vector(podatki$X2.297061)
-histogram_odskodnin <- hist(visinaodskodnine, freq = TRUE, main = "Histogram odÅ¡kodnin")
+histogram_odskodnin <- hist(visinaodskodnine, freq = TRUE, main = "Histogram odškodnin")
 
-lambda = mde(visinaodskodnine, pexp, start = list(rate = 1/200))
-#E = 12.5
-lambda = 3
+pribliznaporazdelitev <- mde(visinaodskodnine, pexp, start = list(rate = 1/200))
+lambda = estimate(pribliznaporazdelitev)
+lambda = 3 #dobi iz ocene? kaj je ocena?
 gostota_eksponentne <- function(x) {lambda * exp(((-1) * lambda * x))}
-#kako dodaÅ¡ krivuljo histogramu? ne s plusom
-hiatogram_odskodnin_in_eksponentna <- histogram_odskodnin + curve(gostota_eksponentne, 0, 7, n = 1000)
+#kako dodaš krivuljo histogramu? ne s plusom
+histogram_odskodnin_in_eksponentna <- histogram_odskodnin #
+histogram_odskodnin_in_eksponentna
+curve(gostota_eksponentne, 0, 7, n = 1000)
+
+velikost = 25
+verjetnost = 1/2
+
+#Bin(p) za n
+
+
 
 '''
 par(mfrow=c(2,2))
 curve(x^3-3*x, -2, 2)
 curve(x^2-2, add = TRUE, col = "violet")
-
-
 curve(sin, -pi, 3*pi)
 plot(cos)
 chippy <- function(x) sin(cos(x)*exp(-x/2))
 curve(chippy, -8, 7, n=2001)
-
 for(ll in c("","x","y","xy"))
   curve(log(1+x), 1,100, log=ll, sub=paste("log=",ll))
-
 #3c ni treba!!!!!!!!!!!!!
